3cb8ffbf6bfa5b177f4366de392d5a4197d4ed9b
chris <512364167@qq.com>
2022-05-07 11:38:00 +0800

    添加题目：189.轮转数组；977.有序数组的平方

---
+++ src/main/java/com/chris/leetcode/tp/topic189/RotateArray.java
@@ -0,0 +1,33 @@
+package com.chris.leetcode.tp.topic189;
+
+/**
+ * @创建人 zhanghui
+ * @创建时间 2022/5/7
+ * @描述 轮转数组
+ */
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        rotate(nums, 3);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+}
