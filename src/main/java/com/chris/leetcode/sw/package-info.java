4996aabb71cc5f6455e3b9a73c35910d101ff46d
chris <512364167@qq.com>
2022-05-11 23:32:22 +0800

    添加题目：3. 无重复字符的最长子串；567. 字符串的排列

---
+++ src/main/java/com/chris/leetcode/sw/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @创建人 zhanghui
+ * @创建时间 2022/5/11
+ * @描述 sliding window 滑动窗口相关
+ */
+package com.chris.leetcode.sw;
---
+++ src/main/java/com/chris/leetcode/sw/topic567/CheckInclusion.java
@@ -0,0 +1,40 @@
+package com.chris.leetcode.sw.topic567;
+
+import java.util.Arrays;
+
+/**
+ * @创建人 zhanghui
+ * @创建时间 2022/5/11
+ * @描述 字符串的排列
+ */
+public class CheckInclusion {
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+        System.out.println(checkInclusion(s1, s2));
+    }
+
+    public static boolean checkInclusion(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        if (m > n) {
+            return false;
+        }
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (int i = 0; i < m; i++) {
+            cnt1[s1.charAt(i) - 'a']++;
+            cnt2[s2.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(cnt1, cnt2)) {
+            return true;
+        }
+        for (int i = m; i < n; i++) {
+            cnt2[s2.charAt(i) - 'a']++;
+            cnt2[s2.charAt(i - m) - 'a']--;
+            if (Arrays.equals(cnt1, cnt2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
